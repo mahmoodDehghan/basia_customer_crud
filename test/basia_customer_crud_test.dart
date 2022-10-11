39b886c0760c07928c25ce092f10e3e9a3d19b04
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2022-10-11 16:33:53 +0330

    versioning

--- test/basia_customer_crud_test.dart
+++ test/basia_customer_crud_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:basia_customer_crud/basia_customer_crud.dart';
+// import 'package:basia_customer_crud/basia_customer_crud.dart';
 
 void main() {
   // test('adds one to input values', () {
