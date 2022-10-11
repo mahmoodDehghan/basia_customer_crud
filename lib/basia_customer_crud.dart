763f16220b96c9d93e0450c83ee701cdfcab6c8a
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2022-10-11 14:49:21 +0330

    export home page

--- lib/basia_customer_crud.dart
+++ lib/basia_customer_crud.dart
@@ -1,7 +1,10 @@
 library basia_customer_crud;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/customer/presentation/page/customer_home_screen.dart'
+    show CustomersHomePage;
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
