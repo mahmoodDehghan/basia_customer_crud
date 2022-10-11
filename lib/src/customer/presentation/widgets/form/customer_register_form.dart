598997dbc612f150df95686a7e653d0e031837b6
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2022-10-11 16:55:10 +0330

    bug fix

--- lib/src/customer/presentation/widgets/form/customer_register_form.dart
+++ lib/src/customer/presentation/widgets/form/customer_register_form.dart
@@ -8,74 +8,74 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_libs/shared_libs.dart';
 
-class CustomerRegisterForm extends GetView<CustomerFormController> {
+class CustomerRegisterForm extends StatelessWidget {
   CustomerRegisterForm({Key? key}) : super(key: key);
 
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    final CustomersHomeController pageController = Get.find();
+    final CustomerFormController pageController = Get.find();
     return Form(
       key: _formKey,
       child: Column(
         children: <Widget>[
           Text(
-            controller.errorMessage.value,
-            style: controller.errorMessageStyle.value,
+            pageController.errorMessage.value,
+            style: pageController.errorMessageStyle.value,
             textAlign: TextAlign.center,
           ),
           const VerticalBlankSpace(),
           CustomerFormTextInput(
-            controller: controller.firstNameController,
-            validator: controller.fillValidation,
+            controller: pageController.firstNameController,
+            validator: pageController.fillValidation,
             label: CustomerStrings.customerFirstName.tr,
             action: TextInputAction.next,
             inputType: TextInputType.text,
           ),
           const VerticalBlankSpace(),
           CustomerFormTextInput(
-            controller: controller.lastNameController,
-            validator: controller.fillValidation,
+            controller: pageController.lastNameController,
+            validator: pageController.fillValidation,
             label: CustomerStrings.customerLastName.tr,
             action: TextInputAction.next,
             inputType: TextInputType.text,
           ),
           const VerticalBlankSpace(),
           CustomerFormTextInput(
-            controller: controller.emailController,
+            controller: pageController.emailController,
             label: CustomerStrings.customerEmail.tr,
-            validator: controller.fillValidation,
+            validator: pageController.fillValidation,
             action: TextInputAction.next,
             inputType: TextInputType.number,
           ),
           const VerticalBlankSpace(),
           CustomerFormTextInput(
-            controller: controller.phoneNumberController,
+            controller: pageController.phoneNumberController,
             label: CustomerStrings.customerPhoneNumber.tr,
-            validator: controller.fillValidation,
+            validator: pageController.fillValidation,
             action: TextInputAction.next,
             inputType: TextInputType.number,
           ),
           const VerticalBlankSpace(),
           CustomerFormTextInput(
-            controller: controller.bankAccountNumberController,
+            controller: pageController.bankAccountNumberController,
             label: CustomerStrings.customerBankAccountNumber.tr,
-            validator: controller.fillValidation,
+            validator: pageController.fillValidation,
             action: TextInputAction.next,
             inputType: TextInputType.number,
           ),
           const VerticalBlankSpace(),
           CustomerBirthDatePicker(
-            choosenDate: controller.birth.value,
-            pickDate: controller.onDatePicked,
-            initialDate: controller.birthDate.value,
+            choosenDate: pageController.birth.value,
+            pickDate: pageController.onDatePicked,
+            initialDate: pageController.birthDate.value,
           ),
           const VerticalBlankSpace(),
           CustomerSubmitPart(
             submitLabel: CustomerStrings.addCustomer,
-            onClear: controller.clearEntries,
-            onSubmit: () => controller.onSubmitForm(key: _formKey),
+            onClear: pageController.clearEntries,
+            onSubmit: () => pageController.onSubmitForm(key: _formKey),
             clearLabel: CustomerStrings.clearEntries,
           ),
         ],
